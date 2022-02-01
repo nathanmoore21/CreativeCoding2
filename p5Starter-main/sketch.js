83fe4463699fa1b25e55adbb7968299de5fc9b4d
nathanmoore21 <n00201743@iadt.ie>
2022-02-01 15:32:01 +0000

    Week1Day1

---
+++ p5Starter-main/sketch.js
@@ -0,0 +1,14 @@
+clap(3);
+function clap(num){
+    for(let i=0;i<num;i++){
+        console.log("hi")
+    }
+}
+
+claps(3,8);
+function claps(startNum, endNum){
+    let loopCount = endNum-startNum
+    for(let i=0;i<=loopCount;i++){
+        console.log("hi" = (startNum+i))
+    }
+}
