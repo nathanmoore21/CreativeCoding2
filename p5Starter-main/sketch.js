ab87710189ba08d5dffbc19eb567b90e5482fca7
nathanmoore21 <n00201743@iadt.ie>
2022-02-02 13:15:29 +0000

    translation

--- p5Starter-main/sketch.js
+++ p5Starter-main/sketch.js
@@ -1,14 +1,42 @@
-clap(3);
-function clap(num){
-    for(let i=0;i<num;i++){
-        console.log("hi")
+let boxWidth = 20;
+let boxHeight = 20;
+let spacing = 5;
+let numBoxs = 10;
+let xOffset = 10;
+let yOffset = 10;
+
+
+function setup(){
+    createCanvas(500,500)
+    background(0);
+}
+
+function draw(){
+    drawBoxs();
+}
+
+function drawBoxs(){
+    fill (255,0,0)
+    noStroke()
+    for (let i=0; i<numBoxs; i++){
+        let totalSpace=boxWidth+spacing;
+        rect (i*totalSpace + xOffset, yOffset, boxWidth, boxHeight)
     }
 }
 
-claps(3,8);
-function claps(startNum, endNum){
+clap(3,8);
+
+function clap(startNum,endNum){
     let loopCount = endNum-startNum
     for(let i=0;i<=loopCount;i++){
-        console.log("hi" = (startNum+i))
+        console.log("Hi " + (startNum+i))
     }
+    return "Done";
 }
+
+function addMeUp(num01,num02){
+    let total = num01 + num02; //statement is between {}
+    console.log("all my work");
+    return total;
+}
+
---
+++ script.js
@@ -0,0 +1,2 @@
+// if statement = condition - if the condition is met perform this
+// = is assign opp. == is comparison opp.
