345c779213628bf74b62496c700600fac126564d
nathanmoore21 <n00201743@iadt.ie>
2022-03-08 18:26:15 +0000

    commit8th

--- p5Starter-main/sketch.js
+++ p5Starter-main/sketch.js
@@ -1,42 +1,12 @@
-let boxWidth = 20;
-let boxHeight = 20;
-let spacing = 5;
-let numBoxs = 10;
-let xOffset = 10;
-let yOffset = 10;
-
-
-function setup(){
-    createCanvas(500,500)
-    background(0);
+function setup() {
+    createCanvas(400, 400);
 }
-
 function draw(){
-    drawBoxs();
+    background(0)
+    
+    push ()
+    translate (width/2, height/2)
+    rotate(PI / 4)
+    rect (0,0,50,10)
+    pop ()
 }
-
-function drawBoxs(){
-    fill (255,0,0)
-    noStroke()
-    for (let i=0; i<numBoxs; i++){
-        let totalSpace=boxWidth+spacing;
-        rect (i*totalSpace + xOffset, yOffset, boxWidth, boxHeight)
-    }
-}
-
-clap(3,8);
-
-function clap(startNum,endNum){
-    let loopCount = endNum-startNum
-    for(let i=0;i<=loopCount;i++){
-        console.log("Hi " + (startNum+i))
-    }
-    return "Done";
-}
-
-function addMeUp(num01,num02){
-    let total = num01 + num02; //statement is between {}
-    console.log("all my work");
-    return total;
-}
-
